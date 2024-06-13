598184eecd6c1f00bb65ceb887ac1b9b141b4886
Thomas Roberts <thomas@becausemarket.com>
2024-06-12 23:00:29 -0400

    use pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    libInjectCss(),
+    dts({ rollupTypes: true })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/index.ts'),
+      name: "dock",
+      formats: ['es'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'tailwindcss', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          tailwindcss: "tailwindcss",
+        },
+      }
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
 })
