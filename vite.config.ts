6ac6dfa149f039236a84d24cc258739540dda381
Thomas Roberts <thomas@becausemarket.com>
2024-08-20 22:54:02 -0400

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/dock-ui/',
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, 'src'), // Adjust the path as needed
+      '~': path.resolve(__dirname, 'src'), // Alias for importing modules easily
     },
   },
   build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'), // or 'src/index.js'
+      name: 'DockUI',
+      fileName: (format) => `dock-ui.${format}.js`, // Output file name based on format
+    },
     rollupOptions: {
-      external: ['~/globals.css'], // Add any other external modules if needed
+      // Specify external dependencies that should not be bundled
+      external: ['react', 'react-dom', '~/globals.css'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
 });
